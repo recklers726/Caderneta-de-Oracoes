8acf59f179d061d27ab56f216f6c2353aed1a306
recklers726 <carretero909@gmail.com>
2025-07-16 21:18:06 -0300

    att final?

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleTexto() {
+  const texto = document.getElementById('textoOculto');
+  texto.classList.toggle('ativo');
+}
+
+function toggleTexto2() {
+  const texto = document.getElementById('textoOculto2');
+  texto.classList.toggle('ativo');
+}
